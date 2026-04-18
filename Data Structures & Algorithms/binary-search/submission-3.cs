1872bed359ffbeed5e081767972a4b50dc8ab403
kirill-kostenetskyi <10336022+kirill-kostenetskyi@users.noreply.github.com>
2026-04-18 19:51:43 -0400

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        var L = 0;
+        var R = nums.Length - 1;
+        var i = nums.Length / 2;
+
+        while(L <= R){
+            if(nums[i] > target){
+                R = i - 1;
+            } else if (nums[i] < target){
+                L = i + 1;
+            } else {
+                return i;
+            }
+            i = (R + L) / 2;
+        }
+        return -1;
+    }
+}
