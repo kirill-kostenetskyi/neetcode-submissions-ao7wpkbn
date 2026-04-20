5ca7731a049eb0e7ff0e4b33e48bf7c52f8c5ced
kirill-kostenetskyi <10336022+kirill-kostenetskyi@users.noreply.github.com>
2026-04-20 19:12:03 -0400

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.cs
@@ -0,0 +1,22 @@
+public class KthLargest {
+
+    private PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+    private int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        foreach(var n in nums){
+            pq.Enqueue(n, n);
+        }
+        while(pq.Count > k){
+            pq.Dequeue();
+        }
+    }
+    
+    public int Add(int val) {
+        pq.Enqueue(val, val);
+        while(pq.Count > k){
+            pq.Dequeue();
+        }
+        return pq.Peek();
+    }
+}
